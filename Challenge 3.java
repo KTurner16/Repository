8cac497c4def8bab2d8452f9c0cdf01feeb80364
Kyle Turner <kylet2014@outlook.com>
2016-09-21 11:28:39 +0100

    Add files via upload
    i play pokemon go everyday

---
+++ Challenge 3.java
@@ -0,0 +1,34 @@
+
+package javaapplication4;
+
+import java.util.Scanner;
+public class JavaApplication4 {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+  
+    
+        Scanner myScanner = new Scanner (System.in);
+        
+        System.out.println("What is your first number?");
+        
+        int num = myScanner.nextInt();
+        
+        System.out.println("What is your second number?");
+        int two = myScanner.nextInt();
+        
+        System.out.println("'" + num + "'" + " divided by " + two + " equals " + num/two );
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+    
+}
